195bcd54a6c566bd7b3c3d9125328bc29f6cbde6
andre-alck <andrealck1@gmail.com>
2023-02-25 15:26:50 -0300

    feat: add aula 09

--- app/lib/src/shared/services/realm/realm_config.dart
+++ app/lib/src/shared/services/realm/realm_config.dart
@@ -1,8 +1,13 @@
 import 'package:listinha/src/shared/services/models/configuration_model.dart';
+import 'package:listinha/src/shared/services/models/task_model.dart';
 import 'package:realm/realm.dart';
 
 LocalConfiguration config = Configuration.local(
-  [ConfigurationModel.schema],
+  [
+    ConfigurationModel.schema,
+    Task.schema,
+    TaskBoard.schema,
+  ],
   initialDataCallback: (realm) {
     realm.add(ConfigurationModel('system'));
   },
